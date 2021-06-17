8e3f32181250324d4bf35464f2c898b2c61051ab
akshay naik <akshaynaik797@gmail.com>
2021-06-17 13:38:53 +0530

    0.0.19

--- test2.py
+++
@@ -1,6 +0,0 @@
-# filename = f"{random.randint(99999, 999999)}.png"
-#                     sleep(1)
-#                     driver.save_screenshot(screenshot_folder + '/' + filename)
-#                     insert_log('', self.transaction_id, self.mss_no, self.data['0']['InsurerID'], self.data['0']['Currentstatus'], step,
-#                                status, message, screenshot_url + filename, value)
-#                     print(i['step'], i['value'])
